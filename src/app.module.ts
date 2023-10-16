067dedd4eb2c8c5a78be9bb005fae234803d0ac4
agarere <edizagarer@gmail.com>
2023-10-16 14:14:41 +0300

    Added configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,23 +6,39 @@ import { CatsController } from './cats/cats.controller';
 import { CatsService } from './cats/cats.service';
 import { Cat } from './cats/cat.entity';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
+// type: "postgres",
+// host: "localhost",
+// port: 5432,
+// username: "postgres",
+// password: "12345",
+// database: "movielistapi",
+// entities: ["dist/**/*.entity{.ts,.js}"],
+// synchronize: true
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: './config.env',
+    }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "postgres",
-      password: "12345",
-      database: "movielistapi",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT) || 3000,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
       entities: ["dist/**/*.entity{.ts,.js}"],
-      synchronize: true
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      }
     }),
     TypeOrmModule.forFeature([Cat]),
+    
     UserModule
   ],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule { }
