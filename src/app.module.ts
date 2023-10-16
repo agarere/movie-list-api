6778a89681db3c9f5b6f41dea4e5270546b5c45d
agarere <edizagarer@gmail.com>
2023-10-16 15:56:51 +0300

    changed db configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,11 +23,11 @@ import { ConfigModule } from '@nestjs/config';
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT) || 3000,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DBNAME,
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       entities: ["dist/**/*.entity{.ts,.js}"],
       synchronize: true,
       ssl: {
