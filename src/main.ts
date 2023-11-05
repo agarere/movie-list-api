2bf99f7b6306c86862cdb80ef38f23bd86bb32a4
agarere <edizagarer@gmail.com>
2023-11-05 15:39:12 +0300

    Added CORS Configs

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,19 @@ async function bootstrap() {
 
   console.log('Node', process.env.NODE_ENV);
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
+  console.log(app);
+
   await app.listen(3000);
 }
 bootstrap();
